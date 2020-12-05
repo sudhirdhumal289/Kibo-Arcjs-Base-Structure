384566e5a0e62378cbaaef5ffda9c0bafb8db062
Sudhir Dhumal <sudhir.d@ignitiv.com>
2020-12-05 14:13:18 +0530

    Changes to process the multiple manifest files on app install.

--- assets/src/core/appInstall.js
+++ assets/src/core/appInstall.js
@@ -2,6 +2,7 @@ const ActionInstaller = require('mozu-action-helpers/installers/actions');
 const CustomRoutesApi = require('mozu-node-sdk/clients/commerce/settings/general/customRouteSettings');
 const PostInstall = require('../post-install');
 const Logger = require('./Logger');
+const PromiseUtils = require('../utils/PromiseUtils');
 
 const logger = new Logger();
 
@@ -27,30 +28,34 @@ function registerCustomRoutes(actionIds, entrypointManifest, context) {
   return tenant.sites.map((site) => new Promise((resolve, reject) => {
     const customRoutesApi = new CustomRoutesApi();
     customRoutesApi.context.site = site.id;
-    customRoutesApi.getCustomRouteSettings().then((currentRoutesConfig) => {
-      const updatedRoutes = currentRoutesConfig ? (currentRoutesConfig.routes || []) : [];
+    customRoutesApi.getCustomRouteSettings()
+      .then((currentRoutesConfig) => {
+        const updatedRoutes = currentRoutesConfig ? (currentRoutesConfig.routes || []) : [];
 
-      actionIds.forEach((actionId) => {
-        if (entrypointManifest[actionId].actionName === 'http.storefront.routes') {
-          const existingRouteConfig = updatedRoutes.find((config) => config.functionId === actionId);
-          const routeConfig = existingRouteConfig || {};
-          routeConfig.template = entrypointManifest[actionId].customRoute || `api/${actionId}`;
-          routeConfig.defaults = {};
-          routeConfig.internalRoute = 'Arcjs';
-          routeConfig.functionId = actionId;
-          routeConfig.mappings = {};
-          routeConfig.validators = {};
+        actionIds.forEach((actionId) => {
+          if (entrypointManifest[actionId].actionName === 'http.storefront.routes') {
+            const existingRouteConfig = updatedRoutes.find((config) => config.functionId === actionId);
+            const routeConfig = existingRouteConfig || {};
+            routeConfig.template = entrypointManifest[actionId].customRoute || `api/${actionId}`;
+            routeConfig.defaults = {};
+            routeConfig.internalRoute = 'Arcjs';
+            routeConfig.functionId = actionId;
+            routeConfig.mappings = {};
+            routeConfig.validators = {};
 
-          if (!existingRouteConfig) {
-            updatedRoutes.push(routeConfig);
+            if (!existingRouteConfig) {
+              updatedRoutes.push(routeConfig);
+            }
           }
-        }
-      });
+        });
 
-      const customRouteConfig = currentRoutesConfig || {};
-      customRouteConfig.routes = updatedRoutes;
-      customRoutesApi.updateCustomRouteSettings(customRouteConfig).then(resolve).catch(reject);
-    }).catch(reject);
+        const customRouteConfig = currentRoutesConfig || {};
+        customRouteConfig.routes = updatedRoutes;
+        customRoutesApi.updateCustomRouteSettings(customRouteConfig)
+          .then(resolve)
+          .catch(reject);
+      })
+      .catch(reject);
   }));
 }
 
@@ -64,14 +69,16 @@ function postInstallAction(context, actions, currentIndex) {
     const returnVal = actions[currentIndex](context);
 
     function nextTick() {
-      postInstallAction(context, actions, currentIndex + 1).then(resolve).catch((error) => {
-        logger.error({
-          message: `Error occurred while running post install action(s). Error: ${error.message}`,
-          stack: error.stack,
-        });
+      postInstallAction(context, actions, currentIndex + 1)
+        .then(resolve)
+        .catch((error) => {
+          logger.error({
+            message: `Error occurred while running post install action(s). Error: ${error.message}`,
+            stack: error.stack,
+          });
 
-        resolve();
-      });
+          resolve();
+        });
     }
 
     if (returnVal && returnVal.then) {
@@ -81,18 +88,19 @@ function postInstallAction(context, actions, currentIndex) {
         } else {
           nextTick();
         }
-      }).catch((error) => {
-        logger.error({
-          message: `Error occurred while running post install action. Error: ${error.message}`,
-          stack: error.stack,
-        });
+      })
+        .catch((error) => {
+          logger.error({
+            message: `Error occurred while running post install action. Error: ${error.message}`,
+            stack: error.stack,
+          });
 
-        if (actions.length === currentIndex + 1) {
-          resolve();
-        } else {
-          nextTick();
-        }
-      });
+          if (actions.length === currentIndex + 1) {
+            resolve();
+          } else {
+            nextTick();
+          }
+        });
     } else {
       nextTick();
     }
@@ -102,31 +110,49 @@ function postInstallAction(context, actions, currentIndex) {
 function postInstall(context, callback) {
   logger.info('Custom route registration completed. Running post-install action(s).');
 
-  postInstallAction(context, PostInstall.postInstallActions, 0).then(() => {
-    logger.info('Post install action(s) completed successfully.');
-    callback();
-  }).catch((error) => {
-    logger.error({
-      message: `Error occurred while running post install action(s). Error: ${error.message}`,
-      stack: error.stack,
-    });
+  postInstallAction(context, PostInstall.postInstallActions, 0)
+    .then(() => {
+      logger.info('Post install action(s) completed successfully.');
+      callback();
+    })
+    .catch((error) => {
+      logger.error({
+        message: `Error occurred while running post install action(s). Error: ${error.message}`,
+        stack: error.stack,
+      });
 
-    callback();
-  });
+      callback();
+    });
+}
+
+function processManifestFile(context, manifestEntry) {
+  return function manifestProcessor() {
+    const actionIds = Object.keys(manifestEntry);
+
+    return new Promise((resolve) => {
+      function postRegisterActions() {
+        Promise.all(registerCustomRoutes(actionIds, manifestEntry, context))
+          .then(() => {
+            postInstall(context, resolve);
+          })
+          .catch(() => {
+            postInstall(context, resolve);
+          });
+      }
+
+      registerActions(actionIds, manifestEntry, context)
+        .then(postRegisterActions)
+        .catch(postRegisterActions);
+    });
+  };
 }
 
 module.exports = (context, callback) => {
   // eslint-disable-next-line global-require
-  const entrypointManifest = require('../bundles/entrypoint.manifest');
-  const actionIds = Object.keys(entrypointManifest);
+  const manifests = require('../bundles');
+  const manifestProcessors = manifests.fileExports.map((manifestEntry) => processManifestFile(context, manifestEntry));
 
-  function postRegisterActions() {
-    Promise.all(registerCustomRoutes(actionIds, entrypointManifest, context)).then(() => {
-      postInstall(context, callback);
-    }).catch(() => {
-      postInstall(context, callback);
-    });
-  }
-
-  registerActions(actionIds, entrypointManifest, context).then(postRegisterActions).catch(postRegisterActions);
+  PromiseUtils.allSettled(manifestProcessors)
+    .then(callback)
+    .catch(callback);
 };
--- assets/src/utils/PromiseUtils.js
+++ assets/src/utils/PromiseUtils.js
@@ -1,3 +1,12 @@
+/**
+ * This function behaves exactly same as the Promise.allSettled with enhancement to handle non-promise input.
+ * If function is present in promises array then that function will be executed sequentially
+ * and its result will be represented same as the the promise result.
+ *
+ * @param promises Array of promises or plain values or functions or mix or all.
+ * @returns {Promise<unknown>} Returns promise which will be fulfilled when
+ * all the promise-array elements are processed.
+ */
 function allSettled(promises) {
   function promiseSettled(promiseIndex) {
     return new Promise((resolve) => {
@@ -11,26 +20,59 @@ function allSettled(promises) {
 
       function nextTick(resp, failed) {
         if (promiseIndex === promises.length - 1) {
-          resolve([{ status: 'fulfilled', value: resp }]);
+          resolve([{
+            status: 'fulfilled',
+            value: resp,
+          }]);
         } else {
-          promiseSettled(promiseIndex + 1).then((chainedResp) => {
-            const respPromise = Array.isArray(resp) ? resp
-              : [failed ? { status: 'rejected', reason: resp } : { status: 'fulfilled', value: resp }];
-            Array.prototype.push.apply(respPromise, chainedResp);
-            resolve(respPromise);
-          }).catch((chainedResp) => {
-            const respPromise = Array.isArray(resp) ? resp
-              : [failed ? { status: 'rejected', reason: resp } : { status: 'fulfilled', value: resp }];
-            Array.prototype.push.apply(respPromise, chainedResp);
-            resolve(respPromise);
-          });
+          promiseSettled(promiseIndex + 1)
+            .then((chainedResp) => {
+              const respPromise = Array.isArray(resp) ? resp
+                : [failed ? {
+                  status: 'rejected',
+                  reason: resp,
+                } : {
+                  status: 'fulfilled',
+                  value: resp,
+                }];
+              Array.prototype.push.apply(respPromise, chainedResp);
+              resolve(respPromise);
+            })
+            .catch((chainedResp) => {
+              const respPromise = Array.isArray(resp) ? resp
+                : [failed ? {
+                  status: 'rejected',
+                  reason: resp,
+                } : {
+                  status: 'fulfilled',
+                  value: resp,
+                }];
+              Array.prototype.push.apply(respPromise, chainedResp);
+              resolve(respPromise);
+            });
         }
       }
 
       if (currentPromise.then) {
-        currentPromise.then(nextTick).catch((e) => {
-          nextTick(e, true);
-        });
+        currentPromise.then(nextTick)
+          .catch((e) => {
+            nextTick(e, true);
+          });
+      } else if (typeof currentPromise === 'function') {
+        try {
+          const fnReturnValue = currentPromise();
+
+          if (fnReturnValue && fnReturnValue.then) {
+            fnReturnValue.then(nextTick)
+              .catch((e) => {
+                nextTick(e, true);
+              });
+          } else {
+            nextTick(fnReturnValue);
+          }
+        } catch (err) {
+          nextTick(err, true);
+        }
       } else {
         nextTick(currentPromise);
       }
